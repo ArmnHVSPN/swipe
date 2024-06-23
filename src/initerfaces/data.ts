4bf94d2fa6520866e38f8b0806e71c175d412770
ArmnHVSPN <ovsepyan786@gmail.com>
2024-06-23 11:52:45 +0400

    add promo section and fix styles

---
+++ src/initerfaces/data.ts
@@ -0,0 +1,25 @@
+
+
+export const achivmentData = [
+    {
+        category: 'Achivment',
+        items: [
+            {
+                title: 'Happy clients',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla interdum aliquet.',
+                count: '1000+'
+            },
+            {
+                title: 'Products created',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla interdum aliquet.',
+                count: '20+'
+            },
+            {
+                title: 'Investments received',
+                descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut fringilla interdum aliquet.',
+                count: '$5M+'
+            }
+            
+        ]
+    }
+]
